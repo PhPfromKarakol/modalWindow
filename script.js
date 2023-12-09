6f26c0d96b1075ed3b61f965c4f59b52d45c0e43
Nurbek Abdykerimov <104121143+PhPfromKarakol@users.noreply.github.com>
2023-12-10 03:24:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const button = document.querySelector('button');
+
+var modal;
+var backdrop;
+
+button.addEventListener('click', showModalHandler);
+
+function showModalHandler() {
+  if (modal) {
+    return;
+  }
+
+  modal = document.createElement('div');
+  modal.className = 'modal';
+
+  const modalText = document.createElement('p');
+  modalText.textContent = 'Are you sure?';
+
+  const modalCancelAction = document.createElement('button');
+  modalCancelAction.textContent = 'No';
+  modalCancelAction.className = 'btn btn--alt';
+  modalCancelAction.addEventListener('click', closeModalHandler);
+
+  const modalConfirmAction = document.createElement('button');
+  modalConfirmAction.textContent = 'No';
+  modalConfirmAction.className = 'btn';
+  modalConfirmAction.addEventListener('click', closeModalHandler);
+
+  modal.append(modalText);
+  modal.append(modalCancelAction);
+  modal.append(modalConfirmAction);
+
+  document.body.append(modal);
+
+  backdrop = document.createElement('div');
+  backdrop.className = 'backdrop';
+
+  backdrop.addEventListener('click', closeModalHandler);
+
+  document.body.append(backdrop);
+}
+
+function closeModalHandler() {
+  modal.remove();
+  modal = null;
+
+  backdrop.remove();
+  backdrop = null;
+}
